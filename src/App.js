a75b61a91dc9cfee88051e87ef2905194ba5bae9
Berk Ezircan <berkezircan@gmail.com>
2023-10-27 20:17:39 +0300

    Add react router to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Box from "@mui/material/Box";
-
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 // Components & Pages
+import ListingPageComponent from "pages/ListingPage";
+import SingleCharacterPage from "pages/SingleCharacterPage";
 import { Navbar } from "./components/Navbar";
 
 const themeLight = createTheme({
@@ -31,13 +33,20 @@ function App() {
   const [light, setLight] = React.useState(true);
   return (
     <div className="App">
-      <ThemeProvider theme={light ? themeLight : themeDark}>
-        <Box lg={{ width: "100%", maxWidth: 500 }}>
-          <CssBaseline />
+      <Router>
+        <ThemeProvider theme={light ? themeLight : themeDark}>
+          <Box lg={{ width: "100%", maxWidth: 500 }}>
+            <CssBaseline />
 
-          <Navbar light={light} setLight={setLight} />
-        </Box>
-      </ThemeProvider>
+            <Navbar light={light} setLight={setLight} />
+          </Box>
+        </ThemeProvider>
+
+        <Routes>
+          <Route path="/" element={<ListingPageComponent />} />
+          <Route path="/character/:id" element={<SingleCharacterPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
