457a87af6fe0395ccb2494c94376fe484ccdfdf6
Berk Ezircan <berkezircan@gmail.com>
2023-10-30 18:56:43 +0300

    Fix on pagination status

--- src/pages/ListingPage/index.jsx
+++ src/pages/ListingPage/index.jsx
@@ -1,57 +1,56 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useRef, useEffect, useState } from "react";
 
-function RickAndMortyCharacters() {
-  const [characters, setCharacters] = useState([]);
-  const [page, setPage] = useState(1);
+import axios from "axios";
+import { CharacterListContainer } from "components/ListContainer";
+import { Box } from "@mui/material";
+
+const ListingPageComponent = () => {
+  const listInnerRef = useRef();
+  const [currPage, setCurrPage] = useState(1);
+  const [prevPage, setPrevPage] = useState(0);
+  const [userList, setUserList] = useState([]);
+  const [accumulator, setAccumulator] = useState(1);
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `https://rickandmortyapi.com/api/character/?page=${page}`
-        );
-        const newCharacters = response.data.results;
-        setCharacters((prevCharacters) => [
-          ...prevCharacters,
-          ...newCharacters,
-        ]);
-      } catch (error) {
-        console.error(error);
-      }
+      const response = await axios.get(
+        `https://rickandmortyapi.com/api/character/?page=${currPage}`
+      );
+
+      setPrevPage(currPage);
+      setUserList([...userList, ...response.data.results]);
     };
 
-    fetchData();
-  }, [page]);
+    if (prevPage !== currPage) {
+      fetchData();
+    }
+  }, [currPage, prevPage, userList]);
 
-  const handleScroll = () => {
-    if (
-      window.innerHeight + document.documentElement.scrollTop ===
-      document.documentElement.offsetHeight
-    ) {
-      setPage((prevPage) => prevPage + 1);
+  const onScroll = () => {
+    if (listInnerRef.current) {
+      const { scrollTop, scrollHeight, clientHeight } = listInnerRef.current;
+
+      if (scrollTop + clientHeight + accumulator >= scrollHeight) {
+        setCurrPage(currPage + 1);
+
+        console.log(accumulator);
+
+        accumulator < 2 && setAccumulator(1 + currPage * 0.01);
+      }
     }
   };
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  console.log(userList.length);
 
   return (
-    <div>
-      <h1>Rick and Morty Characters</h1>
-      <ul>
-        {characters.map((character) => (
-          <li style={{ padding: "20px" }} key={character.id}>
-            {character.name}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Box style={{ marginBottom: "30px" }}>
+      <CharacterListContainer
+        onScroll={onScroll}
+        listInnerRef={listInnerRef}
+        userList={userList}
+      />
+    </Box>
   );
-}
+};
 
-export default RickAndMortyCharacters;
+export default ListingPageComponent;
