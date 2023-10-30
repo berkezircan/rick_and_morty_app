c4e0c4b7bc1e5003b7d22fd4c7a612d954363a0c
Berk Ezircan <berkezircan@gmail.com>
2023-10-30 22:14:34 +0300

    Implement single character page

--- src/pages/SingleCharacterPage/index.jsx
+++ src/pages/SingleCharacterPage/index.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-const SingleCharacterPage = () => {
-  return <div>SingleCharacterPage</div>;
-};
+function SingleCharacterPage({ match }) {
+  const [character, setCharacter] = useState(null);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/character/${id}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setCharacter(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching character:", error);
+      });
+  }, [id]);
+
+  return (
+    <div>
+      {character ? (
+        <div>
+          <h2>{character.name}</h2>
+          <img src={character.image} alt={character.name} />
+          <p>Status: {character.status}</p>
+          <p>Species: {character.species}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
 
 export default SingleCharacterPage;
